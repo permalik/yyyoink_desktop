cd3cbba0338e247745495097f431d39ae2ddaf95
permalik <permalik@protonmail.com>
2025-04-11 14:19:25 -0500

    feat: impl sidebar and pane

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,115 @@
-use iced::widget::{container, text};
-use iced::{Element, Sandbox, Settings};
+use iced::widget::{container, row, text};
+use iced::{color, executor, Color, Length};
+use iced::{Application, Command, Element, Settings, Theme};
+
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Sidebar;
+
+impl Sidebar {
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        Self {}
+    }
+}
 
 #[derive(Default)]
-struct Editor;
+pub struct SidebarStyle;
+
+impl container::StyleSheet for SidebarStyle {
+    type Style = iced::Theme;
+
+    fn appearance(&self, _style: &Self::Style) -> container::Appearance {
+        container::Appearance {
+            background: Some(iced::Background::Color(color!(0x000000))),
+            text_color: Some(Color::WHITE),
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Pane;
+
+impl Pane {
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+#[derive(Default)]
+pub struct PaneStyle;
+
+impl container::StyleSheet for PaneStyle {
+    type Style = iced::Theme;
+
+    fn appearance(&self, _style: &Self::Style) -> container::Appearance {
+        container::Appearance {
+            background: Some(iced::Background::Color(color!(0xF5EBD8))),
+            text_color: Some(Color::BLACK),
+            ..Default::default()
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+struct Editor {
+    sidebar: Sidebar,
+    pane: Pane,
+}
+
+impl Default for Editor {
+    fn default() -> Editor {
+        let sidebar = Sidebar::new();
+        let pane = Pane::new();
+        Editor {
+            sidebar: sidebar,
+            pane: pane,
+        }
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 enum Message {}
 
-impl Sandbox for Editor {
+impl Application for Editor {
+    type Executor = executor::Default;
+    type Flags = ();
     type Message = Message;
+    type Theme = Theme;
 
-    fn new() -> Self {
-        Self::default()
+    fn new(_flags: ()) -> (Editor, Command<Self::Message>) {
+        (Editor::default(), Command::none())
     }
 
     fn title(&self) -> String {
         String::from("Custom Editor")
     }
 
-    fn update(&mut self, message: Message) {}
+    fn update(&mut self, _message: Message) -> Command<Self::Message> {
+        Command::none()
+    }
 
-    fn view(&self) -> Element<Message> {
-        let hello = text("Hello Editor.");
-        container(hello).into()
+    fn view(&self) -> Element<Self::Message> {
+        let sidebar = container(text("Sidebar."))
+            .width(Length::FillPortion(2))
+            .height(Length::Fill)
+            .padding(5)
+            .style(iced::theme::Container::Custom(Box::new(SidebarStyle)));
+        let pane = container(text("Editor Pane."))
+            .width(Length::FillPortion(6))
+            .height(Length::Fill)
+            .padding(5)
+            .style(iced::theme::Container::Custom(Box::new(PaneStyle)));
+
+        let ui = row![sidebar, pane];
+
+        container(ui)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_y()
+            .into()
     }
 }
 
