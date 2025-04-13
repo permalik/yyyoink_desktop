2615ece258d7679bcaf66c8c97433b849a02c022
permalik <permalik@protonmail.com>
2025-04-13 01:19:45 -0500

    feat: write_file

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use capture::capture_pane::CapturePane;
 use capture::capture_sidebar::CaptureSidebar;
 use iced::widget::{button, column as col, container, row, text, text_editor, text_input};
 use iced::{Color, Element, Font, Length, Task};
+use std::io::ErrorKind;
+use std::path::PathBuf;
+use tokio::io::AsyncWriteExt;
 
 pub fn main() -> iced::Result {
     iced::application("Yoink Desktop", Yoink::update, Yoink::view)
@@ -10,11 +13,19 @@ pub fn main() -> iced::Result {
         .run()
 }
 
+#[derive(Debug, Clone)]
+pub enum Error {
+    PermissionDenied,
+    FileNotFound,
+    IoError(ErrorKind),
+}
+
 struct Capture {
     search: String,
     form_topic: String,
     form_subject: String,
     form_content: text_editor::Content,
+    updated_file: Option<String>,
 }
 
 impl Capture {
@@ -24,6 +35,7 @@ impl Capture {
             form_topic: String::new(),
             form_subject: String::new(),
             form_content: text_editor::Content::new(),
+            updated_file: None,
         }
     }
 }
@@ -51,6 +63,7 @@ enum Message {
     CaptureSubjectChanged(String),
     CaptureFormContentChanged(text_editor::Action),
     SubmitCapture,
+    FileOpened(Result<PathBuf, Error>),
 }
 
 impl Yoink {
@@ -82,6 +95,14 @@ impl Yoink {
                 println!("Subject: {}", self.capture.form_subject);
                 let form_content = self.capture.form_content.text();
                 println!("{}", form_content);
+
+                Task::perform(write_file(), Message::FileOpened)
+            }
+            Message::FileOpened(result) => {
+                if let Ok(path) = result {
+                    self.capture.updated_file = Some(path.to_string_lossy().to_string());
+                    println!("Opened/Written to {}", path.display());
+                }
 
                 Task::none()
             }
@@ -170,5 +191,75 @@ impl Yoink {
             .width(Length::Fill)
             .height(Length::Fill)
             .into()
+    }
+}
+
+async fn write_file() -> Result<PathBuf, Error> {
+    let path = PathBuf::from("/Users/tymalik/Docs/Git/markdown/_test.md");
+    let check_path = path.clone();
+
+    let file_exists = tokio::task::spawn_blocking(move || std::fs::metadata(&check_path).is_ok())
+        .await
+        .map_err(|e| {
+            eprintln!("Failed: Unable to determine if file exists. {}", e);
+            e
+        })
+        .expect("Failed: Unable to determine if file exists.");
+
+    if !file_exists {
+        match tokio::fs::write(&path, b"UPDATED").await {
+            Ok(_) => {
+                println!("Wrote to file {}.", path.display());
+                Ok(path)
+            }
+            Err(e) => match e.kind() {
+                ErrorKind::PermissionDenied => {
+                    eprintln!("Permission denied: Failed to write file.");
+                    Err(Error::PermissionDenied)
+                }
+                ErrorKind::NotFound => {
+                    eprintln!("File Not Found: Failed to write file {}.", path.display());
+                    Err(Error::FileNotFound)
+                }
+                kind => {
+                    eprintln!("Failed to write file: {}", e);
+                    Err(Error::IoError(kind))
+                }
+            },
+        }
+    } else {
+        let file = tokio::fs::OpenOptions::new()
+            .append(true)
+            .open(&path)
+            .await
+            .map_err(|e| {
+                eprintln!("Failed: Cannot open file for append. {}", e);
+                match e.kind() {
+                    ErrorKind::PermissionDenied => Error::PermissionDenied,
+                    ErrorKind::NotFound => Error::FileNotFound,
+                    kind => Error::IoError(kind),
+                }
+            })?;
+
+        let mut writer = tokio::io::BufWriter::new(file);
+        let _ = writer.write_all(b"NEWNEW").await.map_err(|e| {
+            eprintln!("Failed: Cannot append to file. {}", e);
+            match e.kind() {
+                ErrorKind::PermissionDenied => Error::PermissionDenied,
+                ErrorKind::NotFound => Error::FileNotFound,
+                kind => Error::IoError(kind),
+            }
+        });
+
+        writer.flush().await.map_err(|e| {
+            eprintln!("Failed: Cannot flush writer. {}", e);
+            match e.kind() {
+                ErrorKind::PermissionDenied => Error::PermissionDenied,
+                ErrorKind::NotFound => Error::FileNotFound,
+                kind => Error::IoError(kind),
+            }
+        })?;
+
+        Ok(path)
     }
 }
